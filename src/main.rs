f6207896defcf7b3c1dcf46532153be1f50edb30
kino-ma <kino.ma.ms@gmail.com>
2020-09-03 16:26:33 +0900

    [update] To distribute private key and de/en-crypt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use rsa::{PublicKey, RSAPrivateKey, RSAPublicKey};
+use rsa::{RSAPrivateKey, RSAPublicKey, BigUint};
+
+use aias_core::crypto::{DistributedRSAPrivKey, RSAPubKey};
 
 fn main() {
     let mut rng = rand::rngs::OsRng;
@@ -7,14 +9,16 @@ fn main() {
         .expect("failed to generate private key");
     let pubkey = RSAPublicKey::from(&privkey);
 
+    let d_privkey = DistributedRSAPrivKey::new(&privkey, &pubkey);
+    let d_pubkey = RSAPubKey { public_key: pubkey };
+
     let message = "hogehoge";
+    let message_biguint = BigUint::from_bytes_le(message.as_bytes());
 
-    let cipher = pubkey.encrypt(&mut rng, rsa::PaddingScheme::PKCS1v15Encrypt, message.as_bytes())
-        .expect("failed to encrypt message");
+    let encrypted = d_pubkey.encrypt_core(message_biguint);
 
-    let decrypted = privkey.decrypt(rsa::PaddingScheme::PKCS1v15Encrypt, &cipher)
-        .expect("failed to decrypt cipher");
-    let decrypted_str = String::from_utf8(decrypted).unwrap();
+    let decrypted = d_privkey.decrypt_core(encrypted);
+    let decrypted_str = String::from_utf8(decrypted.to_bytes_le()).unwrap();
 
     println!("message:\t'{}'", message);
     println!("decrypted:\t'{}'", decrypted_str);
